1a14c2ae0711e3e6f109c0fc8b875a4ad9aea4e8
MP <marc.picco@gmail.com>
2023-01-30 13:38:26 +0100

    Opens a dialog to select a directory

--- src/main.rs
+++ src/main.rs
@@ -49,8 +49,7 @@ fn build_ui(app: &Application) {
 }
 
 
-fn button_text()  -> String{
-    //"Hello, Worldyyy!"      
+fn button_text()  -> String{   
     let current_dir = env::current_dir().unwrap();
     let current_dir_str = current_dir.to_str().unwrap();
     current_dir_str.to_string()
